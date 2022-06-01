09c3bca0f1ac33a27ae341ceecd5e6298d2b6d9d
Geoffroy Guillotte <geoffroy.guillotte@gmail.com>
2022-06-01 20:08:18 +1000

    added code challenge

---
+++ 1-06-22/main.js
@@ -0,0 +1,35 @@
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+function boolToWord( bool ){
+    if (bool == true) {
+      return 'Yes'
+    } else {
+      return 'No';
+    }
+}
+
+function refactored( bool ) {
+    (bool == true) ? 'Yes' : 'No'
+}
+
+
+// Code as fast as you can! You need to double the integer and return it.
+function doubleInteger(i) {
+    return 2 * i
+}
+
+
+// Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+
+// Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+
+// Create a function which translates a given DNA string into RNA.
+
+// For example:
+
+// "GCAT"  =>  "GCAU"
+// The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
+function DNAtoRNA(dna) {  
+    return dna.split('').map(e => e.replace('T', 'U')).join('')
+}
+
