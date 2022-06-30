54a4f6a0fb41288e6201e92d6fd153b7dcceb034
Gaston Palmers <gaston.palmers@gmail.com>
2022-06-30 17:58:42 +1000

    added correction

--- 30-06-22/main.js
+++ 30-06-22/main.js
@@ -9,7 +9,26 @@
 // I is misinterpreted as 1
 // The test cases contain numbers only by mistake.
 
-function correct(string)
-{
-	// your code here
-}
+// Tests
+    // assert.strictEqual(correct("L0ND0N"),"LONDON");
+    // assert.strictEqual(correct("DUBL1N"),"DUBLIN");
+    // assert.strictEqual(correct("51NGAP0RE"),"SINGAPORE");
+    // assert.strictEqual(correct("BUDAPE5T"),"BUDAPEST");
+    // assert.strictEqual(correct("PAR15"),"PARIS");
+
+    function correct(string)
+    {
+        let result = ''
+        for(let i = 0; i < string.length; i++) {
+            if(string[i] == '5' || string[i] == '0' || string[i] == '1') {
+                if(string[i] === '5') result += 'S';
+                if(string[i] === '0') result += 'O';
+                if(string[i] === '1') result += 'I';
+            }
+            else {
+                result += string[i]
+            }
+        }
+        return result
+    
+    }
