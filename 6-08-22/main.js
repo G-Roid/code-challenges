0e6b7dfb94e04358c0c3d5d8a163e8b2e399323c
Gaston Palmers <45578813+G-Roid@users.noreply.github.com>
2022-08-06 18:45:04 +1000

    array challenge

---
+++ 6-08-22/main.js
@@ -0,0 +1,9 @@
+function quickCheck(arr, elem) {
+    // Only change code below this line
+    return arr.indexOf(elem) ? true : false
+
+  
+    // Only change code above this line
+  }
+  
+  console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
