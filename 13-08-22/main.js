9b827ceca1fe8d0d6e04d308ac5f4c7085065bde
Gaston Palmers <gaston.palmers@gmail.com>
2022-08-13 12:27:11 +1000

    added challenge

---
+++ 13-08-22/main.js
@@ -0,0 +1,28 @@
+// Where do I Belong
+// Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted. The returned value should be a number.
+
+// For example, getIndexToIns([1,2,3,4], 1.5) should return 1 because it is greater than 1 (index 0), but less than 2 (index 1).
+
+// Likewise, getIndexToIns([20,3,5], 19) should return 2 because once the array has been sorted it will look like [3,5,20] and 19 is less than 20 (index 2) and greater than 5 (index 1).
+
+
+
+function getIndexToIns(arr, num) {
+    let sort = arr.sort((a, b) => {
+      return a - b
+    })
+  
+  let index = 0
+    for(let i = 0; i < sort.length; i++) {
+      if(sort[i] > num) {
+        index = i - 1 
+        break
+      }
+    }
+    // console.log(sort)
+    return index
+  
+  }
+  
+  console.log(getIndexToIns([40, 60], 50))
+  console.log(getIndexToIns([10, 20, 30, 40, 50], 35))
