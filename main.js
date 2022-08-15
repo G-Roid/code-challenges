a35f1fbddf47cf02b27ef8abaeeaf302362ae076
Gaston Palmers <45578813+G-Roid@users.noreply.github.com>
2022-08-15 15:10:39 +1000

    array slice challenge

--- main.js
+++ main.js
@@ -1,32 +1,21 @@
-// Task:
-// Your task is to write a function which returns the sum of following series upto nth term(parameter).
+// JavaScript Algorithms and Data Structures
+// Basic Algorithm Scripting
+// Chunky Monkey
+// Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
 
-// Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
-// Rules:
-// You need to round the answer to 2 decimal places and return it as String.
-
-// If the given value is 0 then it should return 0.00
-
-// You will only be given Natural Numbers as arguments.
-
-// Examples:(Input --> Output)
-// 1 --> 1 --> "1.00"
-// 2 --> 1 + 1/4 --> "1.25"
-// 5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
-
-
-function sumOfSeries(n) {
-    let result = 0
-    let a = 1
-    let r = 0.25
-
-    for (let i = 0; i < n; i++) {
-        a = a + i * r
-    }
-
-    return a
+function chunkArrayInGroups(arr, size) {
+  let result = []
+  let groups = Math.ceil(arr.length / size)
+  let counter = size;
+  for(let i = 0; i < groups; i++) {
+    let start = i * size
+    let end = i * size + size
+    result.push(arr.slice(start, end))
+  }
+  return result
+  // console.log(result)
 }
 
-console.log(sumOfSeries(1))
-console.log(sumOfSeries(2))
-console.log(sumOfSeries(3))
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
+chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4)
+chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2)
