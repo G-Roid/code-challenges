c96bb121482cba61f53df45a39651ebea8b6ded6
Gaston Palmers <45578813+G-Roid@users.noreply.github.com>
2022-08-16 12:24:42 +1000

    objects challenge

--- main.js
+++ main.js
@@ -1,21 +1,31 @@
 // JavaScript Algorithms and Data Structures
-// Basic Algorithm Scripting
-// Chunky Monkey
-// Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
+// Object Oriented Programming
+// Understand the Prototype Chain
+// All objects in JavaScript (with a few exceptions) have a prototype. Also, an object’s prototype itself is an object.
 
-function chunkArrayInGroups(arr, size) {
-  let result = []
-  let groups = Math.ceil(arr.length / size)
-  let counter = size;
-  for(let i = 0; i < groups; i++) {
-    let start = i * size
-    let end = i * size + size
-    result.push(arr.slice(start, end))
-  }
-  return result
-  // console.log(result)
+// function Bird(name) {
+//   this.name = name;
+// }
+
+// typeof Bird.prototype;
+// Because a prototype is an object, a prototype can have its own prototype! In this case, the prototype of Bird.prototype is Object.prototype:
+
+// Object.prototype.isPrototypeOf(Bird.prototype);
+// How is this useful? You may recall the hasOwnProperty method from a previous challenge:
+
+// let duck = new Bird("Donald");
+// duck.hasOwnProperty("name");
+// The hasOwnProperty method is defined in Object.prototype, which can be accessed by Bird.prototype, which can then be accessed by duck. This is an example of the prototype chain. In this prototype chain, Bird is the supertype for duck, while duck is the subtype. Object is a supertype for both Bird and duck. Object is a supertype for all objects in JavaScript. Therefore, any object can use the hasOwnProperty method.
+
+// Modify the code to show the correct prototype chain.
+
+function Dog(name) {
+  this.name = name;
 }
 
-chunkArrayInGroups(["a", "b", "c", "d"], 2);
-chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4)
-chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2)
+let beagle = new Dog("Snoopy");
+
+Dog.prototype.isPrototypeOf(beagle);  // yields true
+
+// Fix the code below so that it evaluates to true
+Object.prototype.isPrototypeOf(Dog.prototype);
