7b0f1ac4860c2f1ebf8cab8cbf419ab8b17c04de
Gaston Palmers <45578813+G-Roid@users.noreply.github.com>
2022-08-19 07:03:07 +1000

    custom filter method

--- main.js
+++ main.js
@@ -1,22 +1,20 @@
 // The global variable
 const s = [23, 65, 98, 5];
 
-Array.prototype.myMap = function(callback) {
-  const newArray = [];
+Array.prototype.myFilter = function(callback) {
   // Only change code below this line
-  // for(let i = 0; i < this.length; i++) {
-  //   newArray.push(callback(this[i]))
-  // }
-  this.forEach(element => {
-    newArray.push(callback(element))
-  })
+  const newArray = [];
+  for(let i = 0; i < this.length;i++) {
+    if(callback(this[i])) {
+      newArray.push(this[i])
 
+    }
+    
+  }
   // Only change code above this line
   return newArray;
 };
 
-const new_s = s.myMap(function(item) {
-  return item * 2;
+const new_s = s.myFilter(function(item) {
+  return item % 2 === 1;
 });
-
-console.log(new_s)
