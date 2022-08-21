f63c1baf6111ca5091d9d8a0e13089ecac7629eb
Gaston Palmers <45578813+G-Roid@users.noreply.github.com>
2022-08-22 08:46:31 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,40 @@
-// Use the join method (among others) inside the sentensify function to make a sentence from the words in the string str. The function should return a string. For example, I-like-Star-Wars would be converted to I like Star Wars. For this challenge, do not use the replace method.
+// Sum All Numbers in a Range
+// We'll pass you an array of two numbers. Return the sum of those two numbers plus the sum of all the numbers between them. The lowest number will not always come first.
 
+// For example, sumAll([4,1]) should return 10 because sum of all the numbers between 1 and 4 (both inclusive) is 10.
 
-function sentensify(str) {
-  // Only change code below this line
-  return str.split('-').join(' ')
+function sumAll(arr) {
+  //sort the numbers
+  let sortedArray = arr.sort((a, b) => {
+    return a - b
+  })
 
-  // Only change code above this line
+  // let sum = sortedArray.reduce((previous, current) => {
+  //   return previous + current
+  // })
+
+  let between = 0
+  for(let i = sortedArray[0]; i <= sortedArray[1]; i++) {
+    between += i
+  }
+
+  // console.log(sortedArray)
+  // console.log(sum)
+  // console.log(between)
+  return between
+
+}
+// console.log(sumAll([1, 4]))
+sumAll([1, 4]);
+
+function refined(arr) {
+  let min = Math.min(arr[0], arr[1])
+  let max = Math.max(arr[0], arr[1])
+
+  let result = 0;
+  for(let i = min; i <= max; i++) {
+    result += i
+  }
+  return result
 }
 
-console.log(sentensify("May-the-force-be-with-you"))
-sentensify("May-the-force-be-with-you");
