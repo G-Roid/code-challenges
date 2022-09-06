b00db01f81442e6f48deb66677a236fd3f00c001
Gaston Palmers <45578813+G-Roid@users.noreply.github.com>
2022-09-06 15:26:00 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,50 +1,38 @@
-console.log('hello')
-
 // JavaScript Algorithms and Data Structures
 // Intermediate Algorithm Scripting
-// Pig Latin
-// Pig Latin is a way of altering English Words. The rules are as follows:
+// Missing letters
+// Find the missing letter in the passed letter range and return it.
 
-// - If a word begins with a consonant, take the first consonant or consonant cluster, move it to the end of the word, and add ay to it.
+// If all letters are present in the range, return undefined.
 
-// - If a word begins with a vowel, just add way at the end.
+function fearNotLetter(str) {
+  const alphStr = "abcdefghijklmnopqrstuvwxyz";
+  const alphaArray = alphStr.split('');
+  // console.log(alphaArray)
+  // console.log(alphaArray.indexOf('c'))
 
-// Translate the provided string to Pig Latin. Input strings are guaranteed to be English words in all lowercase.
+  let startIndex = alphaArray.indexOf(str[0]);
+  let alphaSubArray = alphaArray.slice(startIndex, startIndex + str.length)
+  
+  console.log(`Position: ${startIndex}`)
+  console.log(alphaSubArray)
 
-function translatePigLatin(str) {
-    return consonantStart(str)
+  return missingOne(str, alphaSubArray)
+
 }
 
-console.log(translatePigLatin("consonant"));
-console.log(translatePigLatin("california"));
-
-function consonantStart(input) {
-    let vowels = ['a', 'e', 'i', 'o', 'u']
-    let result = []
-    let consonantPattern = ''
-
-    if(vowels.includes(input[0])) {
-        result = input + 'way'
-    } else {
-        for(let i = 0; i < input.length; i++) {
-            if(!vowels.includes(input[i].toLowerCase())) {
-                consonantPattern += input[i]
-                if(consonantPattern.length === input.length) {
-                    result = consonantPattern + 'ay'
-                    return result
-                }
-            } else {
-               let newEnding = input.slice(0, i)
-              //  console.log(newEnding)
-               result = input.slice(i) + newEnding + 'ay'
-               break
-            }
-        }
+function missingOne(str, comp) {
+  let result = undefined;
+  for(let i = 0; i < comp.length; i++) {
+    if(!str.includes(comp[i])) {
+      result = comp[i];
+      break;
     }
-    
-    return result
-
+  }
+  return result;
 }
 
-
-// console.log(translatePigLatin("california")) //should return the string aliforniacay.
+// console.log(fearNotLetter("abce"));
+// console.log(fearNotLetter("abcdefghjklmno"))
+// console.log(fearNotLetter("abcdefghijklmnopqrstuvwxyz"));
+console.log(fearNotLetter("stvwx"))
